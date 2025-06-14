b58b25f952635d0cce1417ccfc22dd4148316866
ilyas <ilyassabid86@gmail.com>
2025-06-14 20:38:47 +0100

    printifnot

---
+++ 10%/printifnot/printifnot.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func PrintIf(str string) string {
+	if str == "" || len(str) <= 2 {
+		return "G\n"
+	}
+	return "Invalid Input\n"
+}
+
+func main() {
+	fmt.Print(PrintIf("abcdefz"))
+	fmt.Print(PrintIf("abc"))
+	fmt.Print(PrintIf(""))
+	fmt.Print(PrintIf("14"))
+}
